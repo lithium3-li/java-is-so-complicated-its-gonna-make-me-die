3b62470008c8632015a895e245d7261439d89443
lithium3-li <boring.axe.fund@gmail.com>
2024-06-29 11:49:54 +0300

    Add files via upload

---
+++ my-app.java
@@ -0,0 +1,6 @@
+class maintext {
+    public static void main(String[] args) {
+        System.out.println("java is so fucking complicated i think it's gonna make GOO leak out of my FUCKING ears");
+    }
+}
+// HELP
